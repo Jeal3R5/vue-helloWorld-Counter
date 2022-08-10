aacf460fce902187978d753e7ed7c2cdf19fdd5c
Joan Renfroe <joan.renfroe@gmail.com>
2022-08-09 23:00:50 -0700

    initial commit

---
+++ src/components/Counter.vue
@@ -0,0 +1,25 @@
+<!-- Composition API Method -->
+
+<template>
+  <div>
+    <h1 @click="addOne">{{ counter }}</h1>
+    <h2>{{ cheese }}</h2>
+    <h3 v-for="num in arr" :key="num">{{ num }}</h3>
+  </div>
+</template>
+
+<script>
+import { ref } from "vue";
+
+export default {
+  name: "Counter",
+  props: ["cheese"],
+  setup() {
+    const counter = ref(0);
+
+    const addOne = () => (counter.value += 1);
+
+    return { counter, addOne };
+  },
+};
+</script>
